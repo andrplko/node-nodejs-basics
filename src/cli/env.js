9e48fa91da63ed93dae089d354220750f9c50652
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 09:09:29 +0100

    feat: implement parse env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+import { env } from 'node:process';
+
+env.RSS_name1 = 'value1';
+env.RSS_name2 = 'value2';
+
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const filteredVariables = Object.entries(env)
+    .filter(([key]) => key.startsWith(prefix));
+
+  const formattedVariables = filteredVariables
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  console.log(formattedVariables);
 };
 
 parseEnv();

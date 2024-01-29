ef52d94bead37c1409524324e76e0b18e02b5fb0
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 09:19:31 +0100

    feat: implement compress function

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,33 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream, unlink } from 'node:fs';
+import { cwd, exitCode } from 'node:process';
+import { join } from 'path';
+
 const compress = async () => {
-    // Write your code here 
+  const directoryPath = join(cwd(), 'files');
+  const sourceFile = 'fileToCompress.txt';
+  const destinationFile = 'archive.gz';
+  const sourceFilePath = join(directoryPath, sourceFile);
+  const destinationFilePath = join(directoryPath, destinationFile);
+
+  const gzip = createGzip();
+  const source = createReadStream(sourceFilePath);
+  const destination = createWriteStream(destinationFilePath);
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred:', err);
+      exitCode = 1;
+    } else {
+      unlink(sourceFilePath, (unlinkError) => {
+        if (unlinkError) {
+          console.error('Error removing original file:', unlinkError);
+          exitCode = 1;
+        }
+      });
+    }
+  });
 };
 
 await compress();

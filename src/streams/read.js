bdce17626b84733c9bb799ef95950fef8e66c5ca
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 09:14:58 +0100

    feat: implement stream read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import {
+  createReadStream,
+} from 'node:fs';
+import { cwd } from 'node:process';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const directoryPath = join(cwd(), 'files');
+  const fileName = 'fileToRead.txt';
+  const filePath = join(directoryPath, fileName);
+
+  const stream = createReadStream(filePath, { encoding: 'utf8' });
+
+  stream.on('data', (data) => {
+    stdout.write(`${data}\n`);
+  });
 };
 
 await read();

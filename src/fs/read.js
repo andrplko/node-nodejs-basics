dc458f3f850ace43f076da00189983669c5876a8
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 08:42:41 +0100

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import { readFile } from 'fs/promises';
+import { cwd } from 'node:process';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const directoryPath = join(cwd(), 'files');
+  const fileName = 'fileToRead.txt';
+  const filePath = join(directoryPath, fileName);
+
+  try {
+    const data = await readFile(filePath, { encoding: 'utf-8' });
+
+    console.log(data);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      console.error(err);
+    }
+  }
+
 };
 
 await read();

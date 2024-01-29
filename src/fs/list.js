006f559f5748586026ddb365ee45530f48a72040
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 08:42:26 +0100

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { readdir } from 'fs/promises';
+import { cwd } from 'node:process';
+import { join, parse } from 'path';
+
 const list = async () => {
-    // Write your code here 
+  const directoryPath = join(cwd(), 'files');
+
+  try {
+    const files = await readdir(directoryPath);
+    const arrayOfFilenames = files.map((file) => parse(file).name);
+
+    console.log(arrayOfFilenames);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 await list();

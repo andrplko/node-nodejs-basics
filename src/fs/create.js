89959f53a6c36fad6fd635b23c1438257b4f74fa
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 08:41:35 +0100

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile, access } from 'fs/promises';
+import { cwd } from 'node:process';
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const directoryPath = join(cwd(), 'files');
+  const fileName = 'fresh.txt';
+  const filePath = join(directoryPath, fileName);
+
+  try {
+    await access(filePath);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await writeFile(filePath, 'I am fresh and young');
+      console.log(`File ${fileName} created successfully.`);
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 await create();

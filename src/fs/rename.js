0e9773087451df28c260962efb73031aa2065541
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 08:42:58 +0100

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,33 @@
+import { promises } from 'fs';
+import { cwd } from 'node:process';
+import { join } from 'path';
+
 const rename = async () => {
-    // Write your code here 
+  const directoryPath = join(cwd(), 'files');
+  const oldFileName = 'wrongFilename.txt';
+  const newFilename = 'properFilename.md';
+  const oldFilePath = join(directoryPath, oldFileName);
+  const newFilePath = join(directoryPath, newFilename);
+
+  try {
+    await promises.access(newFilePath);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      try {
+        await promises.access(oldFilePath);
+        const content = await promises.readFile(oldFilePath, { encoding: 'utf-8' });
+        await promises.writeFile(newFilePath, content);
+        await promises.rename(oldFilePath, newFilePath);
+
+        console.log('File renamed successfully.');
+      } catch (err) {
+        throw new Error('FS operation failed');
+      }
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 await rename();

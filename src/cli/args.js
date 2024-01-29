fd444410efb81eb4488ca69681b2a5a0654ce7a7
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 09:09:06 +0100

    feat: implement parse args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,23 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const formattedArgs = argv
+    .slice(2)
+    .reduce((acc, cur, index, array) => {
+      if (index % 2 === 0) {
+        acc.push(`${cur.replace(/-/g, '')} is`);
+      } else {
+        if (index !== array.length - 1) {
+          acc.push(`${cur},`);
+        } else {
+          acc.push(`${cur}`);
+        }
+      }
+      return acc;
+    }, [])
+    .join(' ');
+
+  console.log(formattedArgs);
 };
 
 parseArgs();

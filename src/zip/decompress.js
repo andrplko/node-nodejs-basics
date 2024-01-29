20401f8f8edcb621a00f36252be730a9c9ee7752
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 09:19:56 +0100

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,33 @@
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream, unlink } from 'node:fs';
+import { cwd, exitCode } from 'node:process';
+import { join } from 'path';
+
 const decompress = async () => {
-    // Write your code here 
+  const directoryPath = join(cwd(), 'files');
+  const sourceFile = 'archive.gz';
+  const destinationFile = 'fileToCompress.txt';
+  const sourceFilePath = join(directoryPath, sourceFile);
+  const destinationFilePath = join(directoryPath, destinationFile);
+
+  const gunzip = createGunzip();
+  const source = createReadStream(sourceFilePath);
+  const destination = createWriteStream(destinationFilePath);
+
+  pipeline(source, gunzip, destination, (err) => {
+    if (err) {
+      console.error('An error occurred during decompression:', err);
+      exitCode = 1;
+    } else {
+      unlink(sourceFilePath, (unlinkError) => {
+        if (unlinkError) {
+          console.error('Error removing compressed file:', unlinkError);
+          exitCode = 1;
+        }
+      });
+    }
+  });
 };
 
 await decompress();

bbbc86f75775dec809e720c1a333cee20929ec9d
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 09:11:15 +0100

    feat: implement calculate hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import {
+  createReadStream,
+} from 'node:fs';
+import { cwd } from 'node:process';
+const {
+  createHash,
+} = await import('node:crypto');
+import { join } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const directoryPath = join(cwd(), 'files');
+  const fileName = 'fileToCalculateHashFor.txt';
+  const filePath = join(directoryPath, fileName);
+
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (data) => {
+    hash.update(data);
+  });
+
+  stream.on('end', () => {
+    const hashValue = hash.digest('hex');
+    console.log(hashValue)
+  });
 };
 
 await calculateHash();

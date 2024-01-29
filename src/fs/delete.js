a31201b3cee8701108b55add0c4308aff090fb49
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 08:41:50 +0100

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { rm } from 'fs/promises';
+import { cwd } from 'node:process';
+import { join } from 'path';
+
 const remove = async () => {
-    // Write your code here 
+  const directoryPath = join(cwd(), 'files');
+  const fileName = 'fileToRemove.txt';
+  const filePath = join(directoryPath, fileName);
+
+  try {
+    await rm(filePath);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed');
+    } else {
+      console.error(err);
+    }
+  }
 };
 
 await remove();

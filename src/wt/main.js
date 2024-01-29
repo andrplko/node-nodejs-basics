0da687a4707599491216fc516cc5df086212f380
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 09:17:34 +0100

    feat: implement main thread

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,48 @@
+import { Worker, isMainThread } from 'worker_threads';
+import os from 'os';
+import { cwd } from 'process';
+import { join } from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+  const fileName = 'worker.js';
+  const filePath = join(cwd(), fileName);
+
+  const numCores = os.cpus().length;
+  const results = [];
+
+  const createWorker = (index) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(filePath, { workerData: index + 10 });
+
+      worker.on('message', (result) => {
+        results[index] = result;
+        resolve();
+      });
+
+      worker.on('error', (error) => {
+        results[index] = { status: 'error', data: null };
+        reject(error);
+      });
+
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+    });
+  };
+
+  if (isMainThread) {
+    const workerPromises = Array(numCores)
+      .fill(null)
+      .map((_, index) => createWorker(index));
+
+    try {
+      await Promise.all(workerPromises);
+      console.log('Results:', results);
+    } catch (error) {
+      console.error('Error occurred in one of the workers:', error);
+    }
+  }
 };
 
 await performCalculations();

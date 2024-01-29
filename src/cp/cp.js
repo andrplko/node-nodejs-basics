da46fc6ea064f0e467d1feb8b86aad3880e7f01c
Andrei Paleshka <andrepoleshko@gmail.com>
2024-01-29 09:10:03 +0100

    feat: implement spawn child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,26 @@
+import { spawn } from 'node:child_process';
+import { stdin, stdout, cwd } from 'node:process';
+import { join } from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const directoryPath = join(cwd(), 'files');
+  const fileName = 'script.js';
+  const filePath = join(directoryPath, fileName);
+
+  const childProcess = spawn('node', [filePath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit'],
+  });
+
+  stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.on('data', (data) => {
+    stdout.write(`${data.toString()}\n`);
+  });
+
+  childProcess.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+    stdin.end();
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2"]);

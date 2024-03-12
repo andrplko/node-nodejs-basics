4b24aba850e90b4ec3542a7cb97a2dc58778120f
Andrei Paleshka <andrepoleshko@gmail.com>
2024-03-12 16:39:37 +0100

    refactor: simplify parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -2,10 +2,8 @@ import { env } from 'node:process';
 
 const parseEnv = () => {
   const prefix = 'RSS_';
-  const filteredVariables = Object.entries(env)
-    .filter(([key]) => key.startsWith(prefix));
-
-  const formattedVariables = filteredVariables
+  const formattedVariables = Object.entries(env)
+    .filter(([key]) => key.startsWith(prefix))
     .map(([key, value]) => `${key}=${value}`)
     .join('; ');
 
